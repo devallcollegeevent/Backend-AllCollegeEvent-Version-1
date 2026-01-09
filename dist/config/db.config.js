33799e84ee9735e206a80cd6bf1afcb5d6dcc6b3
pandascrush <velayuthamsiva55@gmail.com>
2026-01-09 10:45:53 +0530

    fix: remove env and build files from repo

--- dist/config/db.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const { PrismaClient } = require("@prisma/client");
-// creating a single Prisma client instance to interact with database
-const prisma = new PrismaClient();
-module.exports = prisma;
